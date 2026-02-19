cf717bb91a995f73c243a65986fa2543b8680bac
KuribohBBQ <134099997+KuribohBBQ@users.noreply.github.com>
2026-02-18 23:20:21 -0800

    Create DB-insertDepartmentsandCollege.sql

---
+++ DB-insertDepartmentsandCollege.sql
@@ -0,0 +1,25 @@
+-- insert data into college table
+INSERT INTO Colleges (AbbreviatedName, FullName)
+VALUES ('CP', 'Cal Poly');
+
+-- insert data into department table
+INSERT INTO Departments (DepartmentName, College) VALUES ('Biological Sciences', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Chemistry & Biochemistry', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Statistics', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Mathematics', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Physics', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Kinesiology & Public Hlth', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('College General Use', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Advising Center', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Deans Office', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Computing Support Services', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Advancement', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Cntr for Cstal Marine Sci', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Health Professions', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Ctr Sci-Math Teacher Educ', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Math Science Teacher Int', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('LSAMP', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('STAR', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Design & Fabrication Fácil', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('School of Education', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Unknown Department', 'CP');
