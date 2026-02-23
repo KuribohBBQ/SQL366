c5c3e40458fe7a033833de168a4563e5cba33561
nicolaschau <nicolas5354@gmail.com>
2026-02-23 13:39:10 -0800

    Fixed insert College script and aligned DB-setup w/ Rel. Mod. Still need logs

--- DB-insertDepartmentsandCollege.sql
+++ DB-insertDepartmentsandCollege.sql
@@ -1,25 +1,37 @@
 -- insert data into college table
 INSERT INTO Colleges (AbbreviatedName, FullName)
-VALUES ('CP', 'Cal Poly');
-
+VALUES ('BCSM', 'Bailey College of Science and Mathematics');
+INSERT INTO Colleges (AbbreviatedName, FullName)
+VALUES ('CENG', 'College of Engineering');
+INSERT INTO Colleges (AbbreviatedName, FullName)
+VALUES ('OCOB', 'Orfalea College of Business');
+INSERT INTO Colleges (AbbreviatedName, FullName)
+VALUES ('CAFES', 'College of Agriculture, Food and Environmental Sciences');
+INSERT INTO Colleges (AbbreviatedName, FullName)
+VALUES ('CAED', 'College of Architecture and Environmental Design');
+INSERT INTO Colleges (AbbreviatedName, FullName)
+VALUES ('CLA', 'College of Liberal Arts');
+INSERT INTO Colleges (AbbreviatedName, FullName)
+VALUES ('UNKN', 'Unknown College');
 -- insert data into department table
-INSERT INTO Departments (DepartmentName, College) VALUES ('Biological Sciences', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Chemistry & Biochemistry', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Statistics', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Mathematics', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Physics', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Kinesiology & Public Hlth', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('College General Use', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Advising Center', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Deans Office', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Computing Support Services', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Advancement', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Cntr for Cstal Marine Sci', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Health Professions', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Ctr Sci-Math Teacher Educ', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Math Science Teacher Int', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('LSAMP', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('STAR', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Design & Fabrication Fácil', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('School of Education', 'CP');
-INSERT INTO Departments (DepartmentName, College) VALUES ('Unknown Department', 'CP');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Liberal Studies', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Biological Sciences', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Chemistry & Biochemistry', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Statistics', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Mathematics', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Physics', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Kinesiology & Public Hlth', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('College General Use', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Advising Center', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Deans Office', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Computing Support Services', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Advancement', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Cntr for Cstal Marine Sci', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Health Professions', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Ctr Sci-Math Teacher Educ', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Math Science Teacher Int', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('LSAMP', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('STAR', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Design & Fabrication Fácil', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('School of Education', 'BCSM');
+INSERT INTO Departments (DepartmentName, College) VALUES ('Unknown Department', 'UNKN');
--- DB-setup.sql
+++ DB-setup.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Colleges (
-  AbbreviatedName VARCHAR(200) NOT NULL,
+  AbbreviatedName VARCHAR(200) NOT NULL UNIQUE,
   FullName VARCHAR(200) NOT NULL,
   PRIMARY KEY (AbbreviatedName)
 );
@@ -7,7 +7,7 @@ CREATE TABLE Colleges (
 CREATE TABLE Departments (
   DepartmentName    VARCHAR(200) NOT NULL,
   College VARCHAR(200) NOT NULL,
-  PRIMARY KEY (DepartmentName)
+  PRIMARY KEY (DepartmentName, College),
   CONSTRAINT fk_departments_college
     FOREIGN KEY (College) REFERENCES Colleges(AbbreviatedName)
     ON UPDATE CASCADE
@@ -42,13 +42,11 @@ CREATE TABLE Floors (
 -- FloorPlans (1:1 with Floors)
 
 CREATE TABLE FloorPlans (
-  BuildingNumber INT NOT NULL,
-  FloorNumber    INT NOT NULL,
   FileName       VARCHAR(500) NOT NULL,
   FilePath       VARCHAR(1000) NOT NULL,
-
-  PRIMARY KEY (BuildingNumber, FloorNumber),
-
+  BuildingNumber INT NOT NULL,
+  FloorNumber    INT NOT NULL,
+  PRIMARY KEY (FileName),
   CONSTRAINT fk_floorplans_floor
     FOREIGN KEY (BuildingNumber, FloorNumber)
     REFERENCES Floors(BuildingNumber, FloorNumber)
@@ -68,13 +66,10 @@ CREATE TABLE RoomType (
 -- Rooms (weak under Buildings)
 CREATE TABLE Rooms (
   BuildingNumber  INT NOT NULL,
-  RoomNumber      INT NOT NULL,
+  RoomNumber      VARCHAR(50) NOT NULL,
   FloorNumber     INT NOT NULL,      
-
-  DepartmentName  VARCHAR(200) NULL,
-  RoomUseCode     INT NULL,
-
-  SquareFeet      INT NULL,
+  RoomUseCode     INT NOT NULL,
+  SquareFeet      FLOAT,
   Notes           VARCHAR(2000) NULL,
   Occupancy       INT NULL,
 
@@ -92,11 +87,6 @@ CREATE TABLE Rooms (
     ON UPDATE CASCADE
     ON DELETE RESTRICT,
 
-  CONSTRAINT fk_rooms_department
-    FOREIGN KEY (DepartmentName) REFERENCES Departments(DepartmentName)
-    ON UPDATE CASCADE
-    ON DELETE SET NULL,
-
   CONSTRAINT fk_rooms_type
     FOREIGN KEY (RoomUseCode) REFERENCES RoomType(RoomUseCode)
     ON UPDATE CASCADE
@@ -105,15 +95,29 @@ CREATE TABLE Rooms (
 
 
 CREATE TABLE RoomImage (
+  FileName VARCHAR(500) NOT NULL,
   BuildingNumber INT NOT NULL,
   RoomNumber INT NOT NULL,
-  
-  ImageURL VARCHAR(500) NOT NULL,
 
-  PRIMARY KEY(BuildingNumber, RoomNumber, ImageURL),
+  PRIMARY KEY(FileName),
 
   CONSTRAINT fk_roomimage_room
     FOREIGN KEY (BuildingNumber, RoomNumber) 
+    REFERENCES Rooms(BuildingNumber, RoomNumber)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE RoomCoordinates (
+  BuildingNum INT NOT NULL,
+  RoomNumber VARCHAR(50) NOT NULL,
+  TopLeftX INT NOT NULL,
+  TopLeftY INT NOT NULL,
+  BottomRightX INT NOT NULL,
+  BottomRightY INT NOT NULL,
+  PRIMARY KEY (BuildingNum, RoomNumber),
+  CONSTRAINT fk_roomcoordinates_room
+    FOREIGN KEY (BuildingNum, RoomNumber) 
     REFERENCES Rooms(BuildingNumber, RoomNumber)
     ON UPDATE CASCADE
     ON DELETE CASCADE
@@ -124,7 +128,7 @@ CREATE TABLE RoomImage (
 CREATE TABLE Equipment (
   EType        VARCHAR(100) NOT NULL,
   EDescription VARCHAR(1000) NULL,
-  Sensitive   BOOLEAN NOT NULL DEFAULT FALSE,
+  Sensitive   VARCHAR(50) NOT NULL DEFAULT 'No',
   PRIMARY KEY (EType)
 );
 
@@ -145,19 +149,15 @@ CREATE TABLE RoomsHaveEquipment (
 );
 
 CREATE TABLE Employees (
+  EmpID        INT NOT NULL AUTO_INCREMENT,
   Email          VARCHAR(320) NOT NULL,
   FullName       VARCHAR(200) NOT NULL,
   DepartmentName VARCHAR(200) NOT NULL,
   College        VARCHAR(200) NOT NULL,
-  PRIMARY KEY (Email),
+  PRIMARY KEY (EmpID),
 
   CONSTRAINT fk_emp_dept
-    FOREIGN KEY (DepartmentName) REFERENCES Departments(DepartmentName)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
-
-  CONSTRAINT fk_emp_college
-    FOREIGN KEY (College) REFERENCES Colleges(AbbreviatedName)
+    FOREIGN KEY (DepartmentName, College) REFERENCES Departments(DepartmentName, College)
     ON UPDATE CASCADE
     ON DELETE RESTRICT
 );
@@ -170,36 +170,17 @@ CREATE TABLE Roles (
   PRIMARY KEY (Role_ID)
 );
 
-CREATE TABLE Permissions (
-  ID          INT NOT NULL,
-  Name        VARCHAR(200) NOT NULL,
-  Description VARCHAR(2000) NULL,
-  PRIMARY KEY (ID)
-);
-
-CREATE TABLE RolePermissions (
-  Role_ID      VARCHAR(50) NOT NULL,
-  PermissionID INT NOT NULL,
-  PRIMARY KEY (Role_ID, PermissionID),
-  CONSTRAINT fk_rp_role
-    FOREIGN KEY (Role_ID) REFERENCES Roles(Role_ID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
-  CONSTRAINT fk_rp_permission
-    FOREIGN KEY (PermissionID) REFERENCES Permissions(ID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
 
 
 -- Users assigned to Roles (M:1)
 
 CREATE TABLE Users (
+  UserID   INT NOT NULL AUTO_INCREMENT,
   Email     VARCHAR(320) NOT NULL,
   Name      VARCHAR(200) NOT NULL,
   Password  VARCHAR(255) NOT NULL,  -- store hash
   Role_ID   VARCHAR(50) NOT NULL,
-  PRIMARY KEY (Email),
+  PRIMARY KEY (UserID),
   CONSTRAINT fk_users_role
     FOREIGN KEY (Role_ID) REFERENCES Roles(Role_ID)
     ON UPDATE CASCADE
@@ -209,353 +190,54 @@ CREATE TABLE Users (
 
 -- Logs (User 1:M Logs, Logs M:1 Rooms)
 
-CREATE TABLE Logs (
-  LogID            INT NOT NULL AUTO_INCREMENT,
-  LogDate          DATE NOT NULL,
-  UserEmail        VARCHAR(320) NOT NULL,
-  PRIMARY KEY (LogID),
-
-  CONSTRAINT fk_logs_user
-    FOREIGN KEY (UserEmail) REFERENCES Users(Email)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-);
-
-CREATE TABLE LogIn (
-  LogID     INT NOT NULL,
-  LoginTime TIME NOT NULL,
-  PRIMARY KEY (LogID),
-  CONSTRAINT fk_login_log
-    FOREIGN KEY (LogID) REFERENCES Logs(LogID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
-
-CREATE TABLE LogOut (
-  LogID      INT NOT NULL,
-  LogoutTime TIME NOT NULL,
-  PRIMARY KEY (LogID),
-  CONSTRAINT fk_logout_log
-    FOREIGN KEY (LogID) REFERENCES Logs(LogID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
-
-CREATE TABLE Colleges (
-  AbbreviatedName VARCHAR(200) NOT NULL,
-  FullName VARCHAR(200) NOT NULL,
-  PRIMARY KEY (AbbreviatedName)
-);
-
-CREATE TABLE Departments (
-  DepartmentName    VARCHAR(200) NOT NULL,
-  College VARCHAR(200) NOT NULL,
-  PRIMARY KEY (DepartmentName),
-  CONSTRAINT fk_departments_college
-    FOREIGN KEY (College) REFERENCES Colleges(AbbreviatedName)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-);
 
 
--- Buildings
-
-CREATE TABLE Buildings (
-  BuildingNumber INT NOT NULL,
-  Name           VARCHAR(200) NOT NULL,
-  NumOfFloors     INT NOT NULL,
-  PRIMARY KEY (BuildingNumber),
-  CONSTRAINT chk_buildings_numfloors
-    CHECK (NumOfFloors > 0)
-);
-
--- Floors (weak under Building)
-
-CREATE TABLE Floors (
-  BuildingNumber INT NOT NULL,
-  FloorNumber    INT NOT NULL,
-  PRIMARY KEY (BuildingNumber, FloorNumber),
-  CONSTRAINT fk_floors_building
-    FOREIGN KEY (BuildingNumber) REFERENCES Buildings(BuildingNumber)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
-
-
--- FloorPlans (1:1 with Floors)
-
-CREATE TABLE FloorPlans (
-  BuildingNumber INT NOT NULL,
-  FloorNumber    INT NOT NULL,
-  FileName       VARCHAR(500) NOT NULL,
-  FilePath       VARCHAR(1000) NOT NULL,
-
-  PRIMARY KEY (BuildingNumber, FloorNumber),
-
-  CONSTRAINT fk_floorplans_floor
-    FOREIGN KEY (BuildingNumber, FloorNumber)
-    REFERENCES Floors(BuildingNumber, FloorNumber)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
-
-
-
--- RoomType
-CREATE TABLE RoomType (
-  RoomUseCode INT NOT NULL,
-  Name VARCHAR(200) NOT NULL,
-  PRIMARY KEY (RoomUseCode)
-);
-
--- Rooms (weak under Buildings)
-CREATE TABLE Rooms (
-  BuildingNumber  INT NOT NULL,
-  RoomNumber      INT NOT NULL,
-  FloorNumber     INT NOT NULL,      
-
-  DepartmentName  VARCHAR(200) NULL,
-  RoomUseCode     INT NULL,
-
-  SquareFeet      INT NULL,
-  Notes           VARCHAR(2000) NULL,
-  Occupancy       INT NULL,
-
-  PRIMARY KEY (BuildingNumber, RoomNumber),
-
-  CONSTRAINT fk_rooms_building
-    FOREIGN KEY (BuildingNumber)
-    REFERENCES Buildings(BuildingNumber)
+-- Relationship Set Tables (M:M or 1:M without participation constraints)
+CREATE TABLE EmployeesAssignedToRooms (
+  EmpID INT NOT NULL,
+  RoomNum VARCHAR(50) NOT NULL,
+  BuildingNum INT NOT NULL,
+  PRIMARY KEY (EmpID, RoomNum, BuildingNum),
+  CONSTRAINT fk_eatr_emp
+    FOREIGN KEY (EmpID) REFERENCES Employees(EmpID)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
-
-  CONSTRAINT fk_rooms_floor
-    FOREIGN KEY (BuildingNumber, FloorNumber)
-    REFERENCES Floors(BuildingNumber, FloorNumber)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
-
-  CONSTRAINT fk_rooms_department
-    FOREIGN KEY (DepartmentName) REFERENCES Departments(DepartmentName)
-    ON UPDATE CASCADE
-    ON DELETE SET NULL,
-
-  CONSTRAINT fk_rooms_type
-    FOREIGN KEY (RoomUseCode) REFERENCES RoomType(RoomUseCode)
-    ON UPDATE CASCADE
-    ON DELETE SET NULL
-);
-
-
-CREATE TABLE RoomImage (
-  BuildingNumber INT NOT NULL,
-  RoomNumber INT NOT NULL,
-  
-  ImageURL VARCHAR(500) NOT NULL,
-
-  PRIMARY KEY(BuildingNumber, RoomNumber, ImageURL),
-
-  CONSTRAINT fk_roomimage_room
-    FOREIGN KEY (BuildingNumber, RoomNumber) 
-    REFERENCES Rooms(BuildingNumber, RoomNumber)
+  CONSTRAINT fk_eatr_room
+    FOREIGN KEY (BuildingNum, RoomNum) REFERENCES Rooms(BuildingNumber, RoomNumber)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );
 
--- Equipment (M:N)
-
-CREATE TABLE Equipment (
-  EType        VARCHAR(100) NOT NULL,
-  EDescription VARCHAR(1000) NULL,
-  Sensitive   BOOLEAN NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (EType)
-);
-
-CREATE TABLE RoomsHaveEquipment (
+CREATE TABLE RoomsAreEquippedWithEquipment (
   BuildingNumber INT NOT NULL,
-  RoomNumber     INT NOT NULL,
+  RoomNumber     VARCHAR(50) NOT NULL,
   EType           VARCHAR(100) NOT NULL,
+  Quantity        INT NOT NULL DEFAULT 1,
   PRIMARY KEY (BuildingNumber, RoomNumber, EType),
-  CONSTRAINT fk_rhe_room
+  CONSTRAINT fk_raew_room
     FOREIGN KEY (BuildingNumber, RoomNumber)
     REFERENCES Rooms(BuildingNumber, RoomNumber)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
-  CONSTRAINT fk_rhe_equipment
+  CONSTRAINT fk_raew_equipment
     FOREIGN KEY (EType) REFERENCES Equipment(EType)
     ON UPDATE CASCADE
     ON DELETE RESTRICT
 );
 
-CREATE TABLE Employees (
-  Email          VARCHAR(320) NOT NULL,
-  FullName       VARCHAR(200) NOT NULL,
+CREATE TABLE RoomsAreAssignedToDepts_Subdiv (
+  BuildingNumber INT NOT NULL,
+  RoomNumber     VARCHAR(50) NOT NULL,
   DepartmentName VARCHAR(200) NOT NULL,
   College        VARCHAR(200) NOT NULL,
-  PRIMARY KEY (Email),
-
-  CONSTRAINT fk_emp_dept
-    FOREIGN KEY (DepartmentName) REFERENCES Departments(DepartmentName)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
-
-  CONSTRAINT fk_emp_college
-    FOREIGN KEY (College) REFERENCES Colleges(AbbreviatedName)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-);
-
--- Roles, Permissions, RolePermissions (Roles given Permissions)
-
-CREATE TABLE Roles (
-  Role_ID   VARCHAR(50) NOT NULL,
-  RoleName  VARCHAR(200) NOT NULL,
-  PRIMARY KEY (Role_ID)
-);
-
-CREATE TABLE Permissions (
-  ID          INT NOT NULL,
-  Name        VARCHAR(200) NOT NULL,
-  Description VARCHAR(2000) NULL,
-  PRIMARY KEY (ID)
-);
-
-CREATE TABLE RolePermissions (
-  Role_ID      VARCHAR(50) NOT NULL,
-  PermissionID INT NOT NULL,
-  PRIMARY KEY (Role_ID, PermissionID),
-  CONSTRAINT fk_rp_role
-    FOREIGN KEY (Role_ID) REFERENCES Roles(Role_ID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
-  CONSTRAINT fk_rp_permission
-    FOREIGN KEY (PermissionID) REFERENCES Permissions(ID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
-
-
--- Users assigned to Roles (M:1)
-
-CREATE TABLE Users (
-  Email     VARCHAR(320) NOT NULL,
-  Name      VARCHAR(200) NOT NULL,
-  Password  VARCHAR(255) NOT NULL,  -- store hash
-  Role_ID   VARCHAR(50) NOT NULL,
-  PRIMARY KEY (Email),
-  CONSTRAINT fk_users_role
-    FOREIGN KEY (Role_ID) REFERENCES Roles(Role_ID)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-);
-
-
--- Logs (User 1:M Logs, Logs M:1 Rooms)
-
-CREATE TABLE Logs (
-  LogId            INT NOT NULL AUTO_INCREMENT,
-  LogDate          DATE NOT NULL,
-  UserEmail        VARCHAR(320) NOT NULL,
-  PRIMARY KEY (LogId),
-
-  CONSTRAINT fk_logs_user
-    FOREIGN KEY (UserEmail) REFERENCES Users(Email)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-);
-
-CREATE TABLE LogIn (
-  LogID     INT NOT NULL,
-  LoginTime TIME NOT NULL,
-  PRIMARY KEY (LogID),
-  CONSTRAINT fk_login_log
-    FOREIGN KEY (LogID) REFERENCES Logs(LogID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
-
-CREATE TABLE LogOut (
-  LogID      INT NOT NULL,
-  LogoutTime TIME NOT NULL,
-  PRIMARY KEY (LogID),
-  CONSTRAINT fk_logout_log
-    FOREIGN KEY (LogID) REFERENCES Logs(LogID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE
-);
-
-CREATE TABLE LogEmplToRoom (
-  LogID             INT NOT NULL,
-  EmployeeEmail     VARCHAR(320) NOT NULL,
-
-  OldBuildingNumber INT NOT NULL,
-  OldRoomNumber     INT NOT NULL,
-
-  NewBuildingNumber INT NOT NULL,
-  NewRoomNumber     INT NOT NULL,
-
-  PRIMARY KEY (LogID),
-
-  CONSTRAINT fk_ler_log
-    FOREIGN KEY (LogID) REFERENCES Logs(LogID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
-
-  CONSTRAINT fk_ler_employee
-    FOREIGN KEY (EmployeeEmail) REFERENCES Employees(Email)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
-
-  -- Old room must exist
-  CONSTRAINT fk_ler_old_room
-    FOREIGN KEY (OldBuildingNumber, OldRoomNumber)
+  PRIMARY KEY (BuildingNumber, RoomNumber, DepartmentName, College),
+  CONSTRAINT fk_raadt_room
+    FOREIGN KEY (BuildingNumber, RoomNumber)
     REFERENCES Rooms(BuildingNumber, RoomNumber)
     ON UPDATE CASCADE
-    ON DELETE RESTRICT,
-
-  -- New room must exist
-  CONSTRAINT fk_ler_new_room
-    FOREIGN KEY (NewBuildingNumber, NewRoomNumber)
-    REFERENCES Rooms(BuildingNumber, RoomNumber)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-);
-
-CREATE TABLE LogDeptToRoom (
-  LogID          INT NOT NULL,
-  OldDepartment  VARCHAR(200) NOT NULL,
-  NewDepartment  VARCHAR(200) NOT NULL,
-  BuildingNumber INT NOT NULL,
-  FloorNumber    INT NOT NULL,
-  RoomNumber     INT NOT NULL,
-  PRIMARY KEY (LogID),
-  CONSTRAINT fk_ldr_log
-    FOREIGN KEY (LogID) REFERENCES Logs(LogID)
-    ON UPDATE CASCADE
     ON DELETE CASCADE,
-  CONSTRAINT fk_ldr_room
-    FOREIGN KEY (BuildingNumber, FloorNumber, RoomNumber)
-    REFERENCES Rooms(BuildingNumber, FloorNumber, RoomNumber)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
-);
-
-CREATE TABLE LogEqpmtToRoom (
-  LogID          INT NOT NULL,
-  EquipmentType  VARCHAR(100) NOT NULL,
-  OldQuantity    INT NOT NULL,
-  NewQuantity    INT NOT NULL,
-  BuildingNumber INT NOT NULL,
-  FloorNumber    INT NOT NULL,
-  RoomNumber     INT NOT NULL,
-  PRIMARY KEY (LogID),
-  CONSTRAINT fk_leq_log
-    FOREIGN KEY (LogID) REFERENCES Logs(LogID)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE,
-  CONSTRAINT fk_leq_room
-    FOREIGN KEY (BuildingNumber, FloorNumber, RoomNumber)
-    REFERENCES Rooms(BuildingNumber, FloorNumber, RoomNumber)
+  CONSTRAINT fk_raadt_dept
+    FOREIGN KEY (DepartmentName, College) REFERENCES Departments(DepartmentName, College)
     ON UPDATE CASCADE
     ON DELETE RESTRICT
 );
