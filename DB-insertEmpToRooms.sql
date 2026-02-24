0670be6ad8dcffbf7480903336e4dd49fe8337bc
nicolaschau <nicolas5354@gmail.com>
2026-02-24 14:11:57 -0800

    Added another script

---
+++ DB-insertEmpToRooms.sql
@@ -0,0 +1,187 @@
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '382', 33
+FROM Employees e
+WHERE e.FullName = 'Thomas Moylan';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '383', 33
+FROM Employees e
+WHERE e.FullName = 'Steven Hendricks';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '387A', 33
+FROM Employees e
+WHERE e.FullName = 'Mallary Greenlee-Wacker';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '387B', 33
+FROM Employees e
+WHERE e.FullName = 'Mallary Greenlee-Wacker';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '387', 33
+FROM Employees e
+WHERE e.FullName = 'Mallary Greenlee-Wacker';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '355C', 33
+FROM Employees e
+WHERE e.FullName = 'Larisa Vredevoe';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '355B', 33
+FROM Employees e
+WHERE e.FullName = 'Charles Knight';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '355A', 33
+FROM Employees e
+WHERE e.FullName = 'Nishi Rajakaruna';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '352A', 33
+FROM Employees e
+WHERE e.FullName = 'Jenn Yost';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '352', 33
+FROM Employees e
+WHERE e.FullName = 'Jenn Yost';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '357A', 33
+FROM Employees e
+WHERE e.FullName = 'Larisa Vredevoe';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '359', 33
+FROM Employees e
+WHERE e.FullName = 'Jenn Yost';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '361', 33
+FROM Employees e
+WHERE e.FullName = 'Jenn Yost';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '362', 33
+FROM Employees e
+WHERE e.FullName = 'Marie  Yeung';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '363', 33
+FROM Employees e
+WHERE e.FullName = 'Juliana Trunzo';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '364', 33
+FROM Employees e
+WHERE e.FullName = 'Charles Knight';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '365', 33
+FROM Employees e
+WHERE e.FullName = 'Delaney Dann';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '366', 33
+FROM Employees e
+WHERE e.FullName = 'Jamie Bunting';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '367', 33
+FROM Employees e
+WHERE e.FullName = 'Nishi Rajakaruna';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '368', 33
+FROM Employees e
+WHERE e.FullName = 'Kara Jew';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '368', 33
+FROM Employees e
+WHERE e.FullName = 'Emily Neal';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '369', 33
+FROM Employees e
+WHERE e.FullName = 'Clinton Francis';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '370', 33
+FROM Employees e
+WHERE e.FullName = 'Gita Kolluru';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '371', 33
+FROM Employees e
+WHERE e.FullName = 'Nikki Adams';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '372', 33
+FROM Employees e
+WHERE e.FullName = 'Ben Ruttenberg';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '373', 33
+FROM Employees e
+WHERE e.FullName = 'Matt Ritter';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '374', 33
+FROM Employees e
+WHERE e.FullName = 'Pat Fidopiastis';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '375', 33
+FROM Employees e
+WHERE e.FullName = 'Heather Liwanag';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '376', 33
+FROM Employees e
+WHERE e.FullName = 'Dena Grossenbacher';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '377', 33
+FROM Employees e
+WHERE e.FullName = 'Jason Blank';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '378', 33
+FROM Employees e
+WHERE e.FullName = 'Crow White';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '379', 33
+FROM Employees e
+WHERE e.FullName = 'Mallary Greenlee-Wacker';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '388', 33
+FROM Employees e
+WHERE e.FullName = 'Jason Blank';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '389', 33
+FROM Employees e
+WHERE e.FullName = 'Michael Black';
+
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '391', 33
+FROM Employees e
+WHERE e.FullName = 'Sandi Clement';
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '392', 33
+FROM Employees e
+WHERE e.FullName = 'Tim Bean';
+
+
+INSERT IGNORE INTO EmployeesAssignedToRooms (EmpID, RoomNumber, BuildingNumber)
+SELECT e.EmpID, '396', 33
+FROM Employees e
+WHERE e.FullName = 'Delaney Dann';
+
