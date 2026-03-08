b0fd338d08582fb0b14f0da626d8fa59552f1928
Euclid <eperegri@calpoly.edu>
2026-03-07 17:03:32 -0800

    add getfloorplans method

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 import mysql.connector
 import os
+import sqlalchemy
 from dotenv import load_dotenv
 from database import get_connection, test_connection
+from pydantic import BaseModel, Field
 
 
 ## pip install these into system
@@ -13,9 +15,42 @@ load_dotenv()
 
 app = FastAPI()
  
+#format for getting list of floor plans
+class FloorPlan(BaseModel):
+    URI: str
+    BuildingName: str
+    BuildingNumber: str
+    FloorNumber: int
+
 @app.get("/")
 def root():
     return {"Running API"}
+
+@app.get("/floorplans", response_model=list[FloorPlan])
+def get_floor_plans():
+    query = """
+    SELECT
+        FileName AS URI,
+        Buildings.Name AS BuildingName,
+        FloorPlans.BuildingNumber AS BuildingNumber,
+        FloorNumber AS FloorNumber
+    FROM FloorPlans
+    JOIN Buildings ON FloorPlans.BuildingNumber = Buildings.BuildingNumber
+    """
+
+    conn = get_connection()
+    try:
+        cur = conn.cursor(dictionary=True)
+        cur.execute(query)
+        rows = cur.fetchall()
+        return rows
+    finally:
+        try:
+            cur.close()
+        except Exception:
+            pass
+        conn.close()
+
 
 #checks if we can connect to database
 @app.get("/db-check")
@@ -24,3 +59,4 @@ def db_check():
         return {"database_connection": "successful"}
     else:
         return {"database_connection": "failed"}
+    
